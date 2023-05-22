2d975abe691dc2a7bf24bfcb94d0bd26932dfed5
jgmdev <jgmdev@gmail.com>
2023-05-22 13:08:44 -0400

    Widget location adaptation

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ local StatusView = require "core.statusview"
 local ReadDoc = require "plugins.scm.readdoc"
 local Git = require "plugins.scm.backend.git"
 local Fossil = require "plugins.scm.backend.fossil"
-local MessageBox = require "libraries.widget.messagebox"
+local MessageBox = require "widget.messagebox"
 
 ---Backends shipped with the plugin.
 ---@type table<string,plugins.scm.backend>
